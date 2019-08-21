37803ddc57c090b31e6ccf81532951af966da12b
smilyamit <54041543+smilyamit@users.noreply.github.com>
2019-08-22 01:52:31 +0200

    Add files via upload
    Random forest is an ensemble method in which a classifier is constructed by combining several different Independent base classifiers. The independence is theoretically enforced by training each base classifier on a training set sampled with replacement from the original training set. This technique is known as bagging, or bootstrap aggregation. In Random Forest, further randomness is introduced by identifying the best split feature from a random subset of available features.

---
+++ Section 9 - Random Forest Regression/random_forest_regression1.py
@@ -0,0 +1,41 @@
+# Random Forest Regression
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting the Random Forest Regression Model to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
+regressor.fit(x,y)
+
+
+# Predicting a new result
+y_pred = regressor.predict([[6.5]])
+
+# Visualising the Regression results
+x_grid = np.arange(min(x), max(x), 0.01)
+x_grid = x_grid.reshape((len(x_grid), 1))
+plt.scatter(x, y, color = 'red')
+plt.plot(x_grid, regressor.predict(x_grid), color = 'blue')
+plt.title('Truth or Bluff (Random Forest Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
